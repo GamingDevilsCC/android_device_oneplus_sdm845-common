261dff516bbd66f3abc08e11a9571d43d7aa2711
Brad Ebinger <breadley@google.com>
2019-03-11 17:38:53 -0700

    X01BD: Enable FEATURE_TELEPHONY_IMS for this device.
    Change-Id: Ibdd8cfad8fa26b147d1c318eb43e4bf181ae4037

--- common.mk
+++ common.mk
@@ -35,7 +35,8 @@ PRODUCT_TARGET_VNDK_VERSION := 28
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.telephony.ims.xml:system/etc/permissions/android.hardware.telephony.ims.xml
 
 # A/B
 AB_OTA_UPDATER := true
