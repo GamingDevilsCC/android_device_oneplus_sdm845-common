511e32273b96383ea4d1165184a24b32319b24f9
LuK1337 <priv.luk@gmail.com>
2019-09-09 21:49:32 +0200

    sdm845-common: Make SELinux great again!
    Change-Id: Id479e031b584b24f79377aa244add20903a42b02

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -123,6 +123,12 @@ BOARD_ROOT_EXTRA_SYMLINKS := \
     /vendor/dsp:/dsp \
     /vendor/firmware_mnt:/firmware
 
+# Telephony
+TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
+
+# SELinux
+PRIVATE_EXCLUDE_BUILD_TEST := true
+
 # Sepolicy
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
 
