2be35dc4efa7daa971787f428f955d4c0764551e
GamingDevilsCC <gamingdevilscc@gmail.com>
2019-10-11 22:36:03 -0400

    Allow missing dependencies
    Signed-off-by: GamingDevilsCC <gamingdevilscc@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -145,3 +145,6 @@ BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flag 2
 
 # Inherit from the proprietary version
 -include vendor/oneplus/sdm845-common/BoardConfigVendor.mk
+
+# Build Error Workaround
+ALLOW_MISSING_DEPENDENCIES := true
